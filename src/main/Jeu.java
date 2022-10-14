e16e71cdaade45aac0662c024a7181a1ec45b0f1
ManderVoronwe <95370877+ManderVoronwe@users.noreply.github.com>
2022-10-14 16:09:17 +0200

    code déplacé

--- src/main/Jeu.java
+++ src/main/Jeu.java
@@ -1,32 +1,33 @@
 package main;
 
-
-import java.awt.*; 
-import javax.swing.*; 
-import java.awt.event.*; 
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 import bouton.*;
 import listeMots.ListeDeMots;
+
 public class Jeu extends JFrame {
 
     public Jeu() {
-        this.setLayout(null); 
-
-        //Chose Word
         ListeDeMots liste = new ListeDeMots("assets/liste-de-mots.txt");
-        liste.chooseWord();
         JLabel mot = new JLabel(liste.getMot());
-        mot.setBounds(20, 30, 300, 50); 
+        this.setLayout(null);
         this.add(mot);
-
-        //add new word button
-        JButton newWord = new JButton("Nouveau mot");
-        newWord.setBounds(400, 0, 120, 30);
-        newWord.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                liste.chooseWord();
-                mot.setText(liste.getMot());
-            }
-        });
+        mot.setBounds(20, 30, 300, 50);
+        // Chose Word
+        /*
+         * //add new word button
+         * JButton newWord = new JButton("Nouveau mot");
+         * newWord.setBounds(400, 0, 120, 30);
+         * newWord.addActionListener(new ActionListener() {
+         * public void actionPerformed(ActionEvent e) {
+         * liste.chooseWord();
+         * mot.setText(liste.getMot());
+         * }
+         * });
+         * this.add(newWord);
+         */
+        NouveauMot newWord = new NouveauMot(mot);
         this.add(newWord);
 
         // Add Quit Button
@@ -39,14 +40,10 @@ public class Jeu extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
 
-
     }
+
     public static void main(String[] args) {
         Jeu jeu = new Jeu();
     }
 
 }
-
-
-
-
