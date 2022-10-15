672ca833ab0299be952b2b4a5773c7788f9b664d
ManderVoronwe <alcor.monllor@gmail.com>
2022-10-15 14:02:06 +0200

    clavier est visible

--- src/visuel/Clavier.java
+++ src/visuel/Clavier.java
@@ -1,17 +1,35 @@
 package visuel;
 
 import bouton.BoutonClavier;
+import java.awt.*;
 
-public class Clavier {
+import javax.swing.border.Border;
+
+public class Clavier extends javax.swing.JPanel {
 
     private BoutonClavier[] boutons;
     private int lastpresed;
 
     public Clavier() {
         boutons = new BoutonClavier[26];
+        this.setLayout(null);
+        this.setBounds(0, 535, 1000, 265);
+        this.setBackground(Color.yellow);
         for (int i = 0; i < 26; i++) {
             boutons[i] = new BoutonClavier(String.valueOf((char) (i + 'A')));
+            this.add(boutons[i]);
         }
+        for (int i = 0; i < 26; i++) {
+            if (i < 10) {
+                boutons[i].setBounds(205 + 60 * i, 40, 50, 50);
+            } else if (i < 19) {
+                boutons[i].setBounds(230 + 60 * (i - 10), 100, 50, 50);
+            } else {
+                boutons[i].setBounds(280 + 60 * (i - 19), 160, 50, 50);
+            }
+            // round the corners
+        }
+
     }
 
     public BoutonClavier[] getBoutons() {
